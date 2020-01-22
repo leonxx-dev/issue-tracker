9e2a2d7e10e853159ed6258a17443030efe33620
Ubuntu <ubuntu@ip-172-31-92-203.ec2.internal>
2020-01-22 18:36:20 +0000

    Added stripe

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -173,6 +173,9 @@ DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
+
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 #Receiving password-reset email in the console.
