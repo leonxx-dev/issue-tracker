e59fd2e9fc5c67a5dada8303625e2209efd5ca5f
Ubuntu <ubuntu@ip-172-31-92-203.ec2.internal>
2020-01-25 13:14:28 +0000

    Ticket types now using values which been inserted manualy. How it was before

--- tickets/forms.py
+++ tickets/forms.py
@@ -1,8 +1,8 @@
 from django import forms
-from .models import Ticket
+from .models import Ticket, TypeName
 
 class TicketForm(forms.ModelForm):
     
     class Meta:
         model = Ticket
-        fields = ['title', 'content', 'ticket_type__id']
+        fields = ['title', 'content', 'ticket_type']
