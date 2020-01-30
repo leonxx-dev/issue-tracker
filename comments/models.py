af73d00f9bcc83251f4d0d962e82c2af883d46e0
Ubuntu <ubuntu@ip-172-31-92-203.ec2.internal>
2020-01-30 15:13:49 +0000

    Models and form been changed

--- comments/models.py
+++ comments/models.py
@@ -7,11 +7,11 @@ class Comment(models.Model):
     
     comment_on = models.ForeignKey(Ticket, on_delete=models.CASCADE, related_name='comments')
     comment_author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    text = models.TextField()
-    published_date = models.DateField(auto_now_add=True, null=True)
+    comment = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
     
     class Meta:
-        ordering = ['-published_date']
+        ordering = ['published_date']
     
     def __str__(self):
         return 'Comment {} by {}'.format(self.text, self.comment_author)
