8e1fcb216c59ed386d452907fb2f672b7c8a1837
Ubuntu <ubuntu@ip-172-31-92-203.ec2.internal>
2020-01-31 17:12:24 +0000

    User can't go to cart if no loged in

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required()
 def view_cart(request):
     """A View that renders the cart contents page"""
     return render(request, 'cart.html')
