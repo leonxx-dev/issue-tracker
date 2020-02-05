656156d51fba588383fc86dc790e086d34f8b0a4
Ubuntu <ubuntu@ip-172-31-92-203.ec2.internal>
2020-02-05 14:08:07 +0000

    comment env

--- issuetracker/settings.py
+++ issuetracker/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import dj_database_url
-from os import path
+# from os import path
 
-if path.exists("env.py"):
-    import env
+# if path.exists("env.py"):
+#     import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
